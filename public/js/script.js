5f1c765232e51634f11f708729ad44ff7a1f411f
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-08-14 11:56:00 +1200

    added our own endpoint to render data

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,66 @@
-
 $(document).ready(function(){
-  console.log('js has loaded');
+  $.ajax({
+    url: 'http://192.168.33.10:3000/people',
+    dataType: 'json',
+    type: 'get',
+    success:function(dataFromServer){
+      console.log(dataFromServer);
+    },
+    error:function(error){
+      console.log(error);
+      console.log('something went wrong');
+    }
+  })
+
+
+  //The Long way
+  // $('.genderRadio').click(function(){
+  //   const gender = $(this).val();
+  //   console.log(gender);
+  //   if(gender === 'Male'){
+  //     $.ajax({
+  //       url: 'http://192.168.33.10:3000/gender/g=male',
+  //       dataType: 'json',
+  //       type: 'get',
+  //       success:function(maleData){
+  //         console.log(maleData);
+  //       },
+  //       error:function(error){
+  //         console.log(error);
+  //         console.log('something went wrong');
+  //       }
+  //     })
+  //   } else {
+  //     $.ajax({
+  //       url: 'http://192.168.33.10:3000/gender/g=female',
+  //       dataType: 'json',
+  //       type: 'get',
+  //       success:function(femaleData){
+  //         console.log(femaleData);
+  //       },
+  //       error:function(error){
+  //         console.log(error);
+  //         console.log('something went wrong');
+  //       }
+  //     })
+  //   }
+  //
+  // })
+
+  $('.genderRadio').click(function(){
+    const gender = $(this).val().toLowerCase();
+    $.ajax({
+      url: `http://192.168.33.10:3000/gender/g=${gender}`,
+      dataType: 'json',
+      type: 'get',
+      success:function(data){
+        console.log(data);
+      },
+      error:function(error){
+        console.log(error);
+        console.log('something went wrong');
+      }
+    })
+  })
+
 })
--- server.js
+++ server.js
@@ -3,21 +3,46 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+const data = require('./MOCK_DATA');
+
 app.use(function(req, res, next){
   console.log(`${req.method} request for ${req.url}`);
   next();
 });
 
 app.use(express.static('./public'));
-// app.get('/about', function(req, res){
-//
-// });
+
+app.get('/aboutProject', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/about.html'));
+});
 
 //Jquery
 //Popper.js
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/popper', express.static(path.join(__dirname, 'node_modules/popper.js/dist/umd')));
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
+
+
+// Make an AJax request in your script.js file to return this data
+app.get('/people', function(req, res){
+  res.json(data);
+});
+
+app.get('/gender/g=:gender', function(req, res){
+  const genderParam = req.params.gender;
+  if( (genderParam === 'male') || (genderParam === 'female') ){
+    let filteredData = [];
+    for (var i = 0; i < data.length; i++) {
+      if(genderParam === data[i].gender.toLowerCase()){
+        filteredData.push(data[i]);
+      }
+    }
+    res.send(filteredData);
+  } else {
+    res.send('Invalid Parameter')
+  }
+});
 
 
 // app.get('/', (req, res) => res.send('Hello World'));
